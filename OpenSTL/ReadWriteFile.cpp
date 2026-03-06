36dbe93ca3547fdaa5c6ffb34328292802d0bb07
Hangzhe Chen <chenhangzhe2906@gmail.com>
2026-03-06 20:34:02 +0800

    first

--- OpenSTL/ReadWriteFile.cpp
+++ OpenSTL/ReadWriteFile.cpp
@@ -1,5 +1,13 @@
 #include "pch.h"
 
+struct Facet
+{
+	float faceNormal[3]; //法向量
+	float v1[3];//坐标
+	float v2[3];//坐标
+	float v3[3];//坐标
+	UINT16 attribute;//附加属性
+};
 
 ReadWriteFile::ReadWriteFile()
 {
@@ -39,12 +47,58 @@ unsigned long ReadWriteFile::GetFileLength(ifstream& is)
 
 bool ReadWriteFile::ReadBinary(ifstream& is, vector<MPoint>& points) const
 {
-	return false;
+	unsigned int nFacet = 0;
+	is.seekg(80, ios::beg);
+	is.read((char*)&nFacet, 4);
+	points.reserve(nFacet * 3);
+	Facet facet;
+	MPoint p;
+	for (int i = 0; i < nFacet; i++) {
+		is.read((char*)&facet, 50);
+		p.Set(facet.v1[0], facet.v1[1], facet.v1[2]);
+		points.push_back(p);
+		p.Set(facet.v2[0], facet.v2[1], facet.v2[2]);
+		points.push_back(p);
+		p.Set(facet.v3[0], facet.v3[1], facet.v3[2]);
+		points.push_back(p);
+	}
+
+	return true;
 }
 
-bool ReadWriteFile::ReadASCII(ifstream& is, vector<MPoint>& points) const
+bool ReadWriteFile::ReadASCII(const CString& pathName, vector<MPoint>& points) const
 {
-	return false;
+	CStdioFile MyFile;
+	MPoint p;
+	if (!MyFile.Open(pathName, CFile::modeRead | CFile::typeText)) {
+		return false;
+	}
+	if (MyFile.GetLength() == 0) {
+		MyFile.Close();
+		return false;
+	}
+	CString Line;
+	while (1) {
+		if (Line.Find(_T("facet normal")) == -1) {
+			break;
+		}
+		for (int i = 0; i < 3; i++) {
+			double x, y, z;
+			MyFile.ReadString(Line);
+			swscanf_s(Line, _T("vertex %lf%lf%lf"), &x, &y, &z);
+			p.Set(x, y, z);
+			points.push_back(p);
+		}
+		MyFile.ReadString(Line);	// 跳过end loop
+		MyFile.ReadString(Line);	//跳过 endfacet
+	}
+	if (points.size() == 0)
+	{
+		inFile.Close();
+		return false;
+	}
+	inFile.Close();
+	return true;
 }
 
 
--- OpenSTL/ReadWriteFile.h
+++ OpenSTL/ReadWriteFile.h
@@ -13,6 +13,6 @@ public:
 	void* ReadSTL(const CString& pathName);
 	unsigned long GetFileLength(ifstream& is);
 	bool ReadBinary(ifstream& is, vector<MPoint>& points) const;
-	bool ReadASCII(ifstream& is, vector<MPoint>& points) const;
+	bool ReadASCII(const CString& pathName, vector<MPoint>& points) const;
 };
 
